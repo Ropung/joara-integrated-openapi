99f51a362a575a645c72829e2536d4e59e4bc3ad
Gihun <563710@naver.com>
2023-06-02 10:11:18 +0900

    check commit

---
+++ joara-springboot-restdocs/src/main/java/com/example/joaraspringbootrestdocs/JoaraSpringbootRestdocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.joaraspringbootrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaraSpringbootRestdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaraSpringbootRestdocsApplication.class, args);
+	}
+
+}
---
+++ joara-springboot-restdocs/src/test/java/com/example/joaraspringbootrestdocs/JoaraSpringbootRestdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joaraspringbootrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaraSpringbootRestdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
